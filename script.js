213d595de7fa8057d46e2c649f500703ecde650e
James Jewitt <jamestjewitt@gmail.com>
2022-07-02 16:38:05 -0400

    Able to add 'tail' element that follows

--- script.js
+++ script.js
@@ -54,7 +54,11 @@ tail.style.gridRow    = snake.style.gridRow
 tail.style.gridColumn = snake.style.gridColumn
 
 /* tail array */
-let tails = [ tail ]
+
+let newTail = document.createElement( 'div' )
+newTail.classList.add( 'box' )
+grid.appendChild( newTail )
+
 
 
 
@@ -97,16 +101,15 @@ document.onkeydown = startMove = ( key ) => {
                     food = boxes[ Math.ceil( Math.random()*900) ]
                     food.style.backgroundColor = 'purple' 
                     
-                    let newTail = document.createElement( 'div' )
-                    newTail.classList.add( 'box' )
-                    grid.appendChild( newTail )
                     newTail.style.backgroundColor = 'pink'
+                    
 
             }
             tail.style.backgroundColor = 'pink'
             tail.style.gridRow = row + 1
             tail.style.gridColumn = snake.style.gridColumn 
 
+            newTail.style.backgroundColor = 'pink'
             newTail.style.gridRow    = row + 2
             newTail.style.gridColumn = snake.style.gridColumn
 
