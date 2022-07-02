d53f3058b10c8875783184364207e0eeba74588f
James Jewitt <jamestjewitt@gmail.com>
2022-07-02 08:25:31 -0400

    Simple functionality - tiles placed in loop, single tile arrow-key movement

--- script.js
+++ script.js
@@ -1,40 +1,102 @@
-const API_KEY = '7bcc0e7fc8f9bfd152d68c486ea8c927'
-const DOMAIN = 'https://api.themoviedb.org/3'
-const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
 
-let searchInput = document.getElementById( 'search-input' )
-const searchButton = document.getElementById( 'search' )
-let movieListDisplay = document.querySelector( '.movie-list' )
+/* defining variables */
 
-const renderList = ( section, foundMoviesArray ) => {
-    for ( let i = 0; i < foundMoviesArray.length; i++ ) {
-        let newDiv = document.createElement( 'div' )
-        let newTitle = document.createElement( 'p' )
-        let newPoster = document.createElement( 'div' )
-        newTitle.innerText = foundMoviesArray[i][0]
-        if ( foundMoviesArray[i][1] != 'poster not found' ) {
-            newPoster.innerHTML = `<img src=${IMAGE_BASE_PATH+foundMoviesArray[i][1]}>`
-        }
-        else { newPoster.innerText = foundMoviesArray[i][1] }
-        newDiv.appendChild( newTitle )
-        newDiv.appendChild( newPoster )
-        section.appendChild( newDiv )
-    }
+let grid = document.querySelector( 'div' )
+let box = document.createElement( 'div' )
+box.classList.add( 'box' )
+let boxes = []
+let indices = []
+
+let row = 0
+let column = 0
+let start = false
+
+
+
+
+/* appending tablets to game board */
+for ( let i = 0; i < 30*30; i++ ) {
+    let box = document.createElement( 'div' )
+    box.classList.add( 'box' )
+    boxes.push( box )
+    grid.appendChild( boxes[i] )
 }
 
-searchButton.addEventListener( 'click', async () => {
-
-    if ( movieListDisplay.childNodes ) {
-        movieListDisplay.innerHTML = ''
+/* placing all of those tablets in coordinate location */
+let b = -1
+for ( let r = 1; r < 31; r++ ) {
+    for ( let c = 1; c < 31; c++ ) {
+        //console.log(`ROW = ${ r }      COLUMN = ${ c }`)
+        b += 1
+        boxes[b].style.gridRow     =  r
+        boxes[b].style.gridColumn  =  c
+        indices.push([ r,c ])
     }
-    let movieToFind = searchInput.value;
-    let moviesFound = await axios.get(`https://api.themoviedb.org/3/search/movie?query=[${movieToFind}]&api_key=${API_KEY}`)
-    let titlesAndPosters = []
-    moviesFound.data.results.map( item => item.poster_path != null ? titlesAndPosters.push([item.original_title,item.poster_path]) : titlesAndPosters.push([item.original_title,'poster not found'] ))
-    console.log( titlesAndPosters )
-    renderList( movieListDisplay, titlesAndPosters )
-    searchInput.value = ''
-})
+}
+ 
+console.log( grid.children )
+
+/* make start */
+
+let snake = boxes[667]
+snake.style.backgroundColor = 'black';
+
+
+
+document.onkeydown = startMove = ( key ) => {
+
+
+    if ( start === false ) {
+        const replaceStart = document.createElement( 'div' )
+        replaceStart.classList.add( 'box' )
+        grid.appendChild( replaceStart )
+        replaceStart.style.gridRow = 23
+        replaceStart.style.gridColumn = 8
+    }
+    else { 
+        console.log( 'started' ) 
+    }
+
+    start = true 
+
+    if ( key.keyCode === 38 ) {
+        clearInterval()
+        let up = () => {
+            row = parseInt(snake.style.gridRow)-1
+            snake.style.gridRow = row
+        }
+        setInterval(up,100) 
+    }
+    else if ( key.keyCode === 39 ) {
+        let right  = () => {
+            column = parseInt(snake.style.gridColumn)+1
+            snake.style.gridColumn = column
+        }
+        setInterval(right,100) 
+    }
+    else if ( key.keyCode === 37 ) {
+        let left = () => { 
+            column = parseInt(snake.style.gridColumn)-1
+            snake.style.gridColumn = column
+        }
+        setInterval( left,100 )
+    }
+    else if ( key.keyCode === 40 ) {
+        let down = () => { 
+            row = parseInt(snake.style.gridRow)+1
+            snake.style.gridRow = row
+        }
+        setInterval( down,100 )
+    }
+    else { console.log( 'User pressed non-arrow key' ) }
+    
+ 
+
+}
+
+
+
+    
 
 
 
@@ -42,3 +104,4 @@ searchButton.addEventListener( 'click', async () => {
 
 
 
+ 
