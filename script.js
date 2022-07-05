76c3e0d20d6e138c4ab94f81a1bb82b7a2ad201e
James Jewitt <jamestjewitt@gmail.com>
2022-07-05 00:03:46 -0400

    Achieve angular movement through path array which say snake's path and replays it on tail elements

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ box.classList.add( 'box' )
 let boxes = []
 let indices = []
 
-let row = 0
-let column = 0
+
 let start = false
 
 /* functions  */ 
@@ -39,19 +38,19 @@ for ( let r = 1; r < 31; r++ ) {
 
 /* make 'power-ups' */
 let food = boxes[ Math.ceil( Math.random()*900) ]
-food.style.backgroundColor = 'white' 
+food.style.backgroundColor = 'green' 
 
 /* make snake 'head' and place at starting tile */
-let snake = boxes[667]
-snake.style.backgroundColor = 'black';
+let head = boxes[667]
+head.style.backgroundColor = 'black';
 
 /* make tail */
 let tail = document.createElement( 'div' )
 tail.classList.add( 'box' )
 grid.appendChild( tail )
 tail.style.backgroundColor = 'transparent'
-tail.style.gridRow    = snake.style.gridRow
-tail.style.gridColumn = snake.style.gridColumn
+tail.style.gridRow    = head.style.gridRow
+tail.style.gridColumn = head.style.gridColumn
 
 /* tail array */
 
@@ -62,20 +61,178 @@ for ( let i = 0; i < 30*30; i++ ) {
      tailsArray[i].classList.add( 'box' )
      tailsArray[i].style.backgroundColor = 'transparent'
 }
-console.log( tailsArray )
+//console.log( tailsArray )
+
+let elements = []
+for ( let i = 0; i < 30*30; i++ ) {
+     elements.push( document.createElement( 'div' ) )
+     grid.appendChild( elements[i] )
+     elements[i].classList.add( 'box' )
+     elements[i].style.backgroundColor = 'transparent'
+}
+
+elements[0].style.gridRow    = '15'
+elements[0].style.gridColumn = '7'
+elements[0].style.backgroundColor = 'blue'
+console.log( elements )
 
 
-let snakeLength = 0
-let passes = 0
-let fromRight = false
-let fromLeft  = false
 
+let snakeLength = 1
+// let passes = 0
+// let fromRight = false
+// let fromLeft  = false
+let path = [ ]
+
+let row = 0
+let column = 0
+
+ 
+console.log( row, column )
 
 
 document.onkeydown = startMove = ( key ) => {
-    console.log( key.keyCode )
+    
+    if ( key.keyCode === 38 ) {
+
+        //otherMovements.forEach( item => clearInterval( item ) )
+        try { clearInterval( moveDown ) } 
+        catch ( error ) { }
+        try { clearInterval( moveLeft ) } 
+        catch ( error ) { }
+        try { clearInterval( moveRight ) } 
+        catch ( error ) { }
+
+        moveUp  = setInterval( () => {
+            row     = parseInt( elements[0].style.gridRow ) - 1
+            column  = parseInt( elements[0].style.gridColumn )
+            path.unshift([ row , column ])
+
+            for( let i = 0; i < snakeLength; i++ ) {
+                elements[i].style.backgroundColor = 'red'
+                elements[i].style.gridRow         = path[i][0]
+                elements[i].style.gridColumn      = path[i][1]
+            }
+
+            if( elements[0].style.gridRow === food.style.gridRow &&
+                elements[0].style.gridColumn=== food.style.gridColumn ){
+
+                food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
+                food = boxes[ Math.ceil( Math.random()*900) ]
+                food.style.backgroundColor = 'purple'
+                snakeLength += 1
+            }
+    },100)}
+    if ( key.keyCode === 40 ) {
+
+        try { clearInterval( moveUp ) } 
+        catch ( error ) { }
+        try { clearInterval( moveLeft ) } 
+        catch ( error ) { }
+        try { clearInterval( moveRight ) } 
+        catch ( error ) { }
+
+        moveDown  = setInterval( () => {
+            row     = parseInt( elements[0].style.gridRow ) + 1
+            column  = parseInt( elements[0].style.gridColumn )
+            path.unshift([ row , column ])
+
+            for( let i = 0; i < snakeLength; i++ ) {
+                elements[i].style.backgroundColor = 'red'
+                elements[i].style.gridRow         = path[i][0]
+                elements[i].style.gridColumn      = path[i][1]
+            }
+
+            if( elements[0].style.gridRow === food.style.gridRow &&
+                elements[0].style.gridColumn=== food.style.gridColumn ){
+
+                food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
+                food = boxes[ Math.ceil( Math.random()*900) ]
+                food.style.backgroundColor = 'purple'
+                snakeLength += 1
+            }
+    },100)}
+    if ( key.keyCode === 37 ) {
+
+        try { clearInterval( moveUp ) } 
+        catch ( error ) { }
+        try { clearInterval( moveDown ) } 
+        catch ( error ) { }
+        try { clearInterval( moveRight ) } 
+        catch ( error ) { }
 
 
+        moveLeft  = setInterval( () => {
+            row     = parseInt( elements[0].style.gridRow    )  
+            column  = parseInt( elements[0].style.gridColumn ) - 1
+            path.unshift([ row , column ])
+
+            for( let i = 0; i < snakeLength; i++ ) {
+                elements[i].style.backgroundColor = 'red'
+                elements[i].style.gridRow         = path[i][0]
+                elements[i].style.gridColumn      = path[i][1]
+            }
+
+            if( elements[0].style.gridRow === food.style.gridRow &&
+                elements[0].style.gridColumn=== food.style.gridColumn ){
+
+                food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
+                food = boxes[ Math.ceil( Math.random()*900) ]
+                food.style.backgroundColor = 'purple'
+                snakeLength += 1
+            }
+    },100)}
+    if ( key.keyCode === 39 ) {
+
+        try { clearInterval( moveUp ) } 
+        catch ( error ) { }
+        try { clearInterval( moveDown ) } 
+        catch ( error ) { }
+        try { clearInterval( moveLeft ) } 
+        catch ( error ) { }
+
+        moveRight  = setInterval( () => {
+            row     = parseInt( elements[0].style.gridRow    )
+            column  = parseInt( elements[0].style.gridColumn ) + 1
+            path.unshift([ row , column ])
+
+            for( let i = 0; i < snakeLength; i++ ) {
+                elements[i].style.backgroundColor = 'red'
+                elements[i].style.gridRow         = path[i][0]
+                elements[i].style.gridColumn      = path[i][1]
+            }
+
+            if( elements[0].style.gridRow    === food.style.gridRow &&
+                elements[0].style.gridColumn === food.style.gridColumn ){
+
+                food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
+                food = boxes[ Math.ceil( Math.random()*900) ]
+                food.style.backgroundColor = 'purple'
+                snakeLength += 1
+            }
+    },100)}
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+document.onkeydown = startMove = ( key ) => {
+    //console.log( key.keyCode )
     if ( start === false ) {
         const replaceStart = document.createElement( 'div' )
         replaceStart.classList.add( 'box' )
@@ -101,38 +258,95 @@ document.onkeydown = startMove = ( key ) => {
             clearInterval( moveRight )
         } catch ( error ) {
         }
-        if ( tailsArray[1].style.gridColumn < snake.style.gridColumn ) {
-            console.log( 'recently moving right -> ' )
-            fromRight = true
-            fromLeft = false
-        }
-        else if ( tailsArray[1].style.gridColumn > snake.style.gridColumn ) {
-            console.log( 'recently moving left <- ' )
-            fromLeft = true
-            fromRight = false
-        }
-        else { 
-            fromRight = false
-            fromLeft  = false
-        }
-        passes = 0
+        // if ( tailsArray[1].style.gridColumn < snake.style.gridColumn ) {
+        //     ///console.log( 'recently moving right -> ' )
+        //     ///fromRight = true
+        //     fromLeft = false
+        // }
+        // else if ( tailsArray[1].style.gridColumn > snake.style.gridColumn ) {
+        //     console.log( 'recently moving left <- ' )
+        //     fromLeft = true
+        //     fromRight = false
+        // }
+        // else { 
+        //     fromRight = false
+        //     fromLeft  = false
+        // }
+
+
+
+
+
+
+
+
         moveUp = setInterval( () => {
-            
-            passes += 1
-            row = parseInt( snake.style.gridRow ) - 1
-            snake.style.gridRow = row
+            row                       = parseInt( elements[0].gridRow ) - 1
+            column                    = parseInt( elements[0].gridRow )
+            path.unshift( [ row , column ] )
 
-            if ( snake.style.gridRow === food.style.gridRow &&
-                 snake.style.gridColumn=== food.style.gridColumn ){
+            for ( let i = 0; i < snakeLength; i++ ) {
 
-                    food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
-                    food = boxes[ Math.ceil( Math.random()*900) ]
-                    food.style.backgroundColor = 'purple'
-
-                    snakeLength += 1
+                elements[i].style.backgroundColor = 'red'
+                elements[i].style.gridRow         = path[i][0]
+                elements[i].style.gridColumn      = path[i][1]
             }
+            if ( head.style.gridRow === food.style.gridRow &&
+                head.style.gridColumn=== food.style.gridColumn ){
 
-            /* this only achieves a right angle when traveling from right to up */
+                   food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
+                   food = boxes[ Math.ceil( Math.random()*900) ]
+                   food.style.backgroundColor = 'purple'
+
+                   snakeLength += 1
+           }
+        },100)
+        
+
+
+
+
+
+
+
+
+
+        //passes = 0
+        //moveUp = setInterval( () => {
+            
+            //passes += 1
+            //row = parseInt( snake.style.gridRow ) - 1
+            //snake.style.gridRow = row
+
+            // decreasing rows forces movement upwards
+            //row                       = parseInt( elements[0].gridRow ) - 1
+            //column                    = parseInt( elements[0].gridRow )
+            //path.unshift( [ row , column ] )
+
+            //for ( let i = 0; i < snakeLength; i++ ) {
+
+                //elements[i].style.gridRow    = path[i][0]
+                //elements[i].style.gridColumn = path[i][1]
+           // }
+
+
+            // eating power-ups
+            //if ( head.style.gridRow === food.style.gridRow &&
+                // head.style.gridColumn=== food.style.gridColumn ){
+
+                   // food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
+                   // food = boxes[ Math.ceil( Math.random()*900) ]
+                  //  food.style.backgroundColor = 'purple'
+
+                   // snakeLength += 1
+          //  }
+
+
+
+
+
+
+            /* this only achieves a right angle when traveling from right to up *//*
             if ( fromRight ) {
                 for ( t = 1; t <= snakeLength; t++  ) {
 
@@ -145,7 +359,7 @@ document.onkeydown = startMove = ( key ) => {
                         tailsArray[t].style.gridRow = row + t
                     }
 
-                    if ( tailsArray[t].style.gridColumn !=  snake.style.gridColumn ) {
+                    if ( tailsArray[t].style.gridColumn !=  head.style.gridColumn ) {
                         tailsArray[t].style.gridColumn = column + ( passes - t )
                     }
                     else {
@@ -166,7 +380,7 @@ document.onkeydown = startMove = ( key ) => {
                         tailsArray[t].style.gridRow = row + t
                     }
 
-                    if ( tailsArray[t].style.gridColumn !=  snake.style.gridColumn ) {
+                    if ( tailsArray[t].style.gridColumn !=  head.style.gridColumn ) {
                         tailsArray[t].style.gridColumn = column - ( passes - t )
                     }
                     else {
@@ -176,12 +390,8 @@ document.onkeydown = startMove = ( key ) => {
                 }
             }
 
-
-
-
-
         },100)
-        snake.style.backgroundColor = 'blue'
+        head.style.backgroundColor = 'blue'
 
     }
 
@@ -202,10 +412,10 @@ document.onkeydown = startMove = ( key ) => {
             console.log( error )
         }
         moveRight = setInterval( () => {
-            column = parseInt( snake.style.gridColumn ) + 1
-            snake.style.gridColumn = column
-            if ( snake.style.gridRow === food.style.gridRow &&
-                 snake.style.gridColumn=== food.style.gridColumn ){
+            column = parseInt( head.style.gridColumn ) + 1
+            head.style.gridColumn = column
+            if ( head.style.gridRow === food.style.gridRow &&
+                 head.style.gridColumn=== food.style.gridColumn ){
                   food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
                   food = boxes[ Math.ceil( Math.random()*900) ]
                   food.style.backgroundColor = 'purple' 
@@ -213,31 +423,19 @@ document.onkeydown = startMove = ( key ) => {
                   snakeLength += 1
             }
 
-
-
-
-
-
             for ( let i = 1; i <= snakeLength; i++ ) {
                 tailsArray[i].style.backgroundColor    = 'pink'
-                tailsArray[i].style.gridRow            = snake.style.gridRow
+                tailsArray[i].style.gridRow            = head.style.gridRow
                 tailsArray[i].style.gridColumn         = column - i
             }
 
 
+       
+       // },100)
+       // head.style.backgroundColor = 'green'
+       // }
 
-
-
-
-
-
-
-
-        },100)
-        snake.style.backgroundColor = 'green'
-    }
-
-    else if ( key.keyCode === 37 ) {
+        if ( key.keyCode === 37 ) {
         try {
             clearInterval( moveUp )
         } catch ( error ) {
@@ -255,9 +453,9 @@ document.onkeydown = startMove = ( key ) => {
         }
         moveLeft = setInterval( () => {
             column = parseInt( snake.style.gridColumn ) - 1
-            snake.style.gridColumn = column
-            if ( snake.style.gridRow === food.style.gridRow &&
-                 snake.style.gridColumn=== food.style.gridColumn ){
+            head.style.gridColumn = column
+            if ( head.style.gridRow === food.style.gridRow &&
+                 head.style.gridColumn=== food.style.gridColumn ){
                    food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
                    food = boxes[ Math.ceil( Math.random()*900) ]
                    food.style.backgroundColor = 'purple' 
@@ -266,11 +464,11 @@ document.onkeydown = startMove = ( key ) => {
             }
             for ( let i = 1; i <= snakeLength; i++ ) {
                 tailsArray[i].style.backgroundColor    = 'pink'
-                tailsArray[i].style.gridRow            = snake.style.gridRow
+                tailsArray[i].style.gridRow            = head.style.gridRow
                 tailsArray[i].style.gridColumn         = column + i
             }
         },100)
-        snake.style.backgroundColor = 'red'
+        head.style.backgroundColor = 'red'
     }
 
     else if ( key.keyCode === 40 ) {
@@ -293,10 +491,10 @@ document.onkeydown = startMove = ( key ) => {
         moveDown = setInterval( () => {
             index += 1
             console.log( index )
-            row = parseInt( snake.style.gridRow ) + 1
-            snake.style.gridRow = row
-            if ( snake.style.gridRow === food.style.gridRow &&
-                 snake.style.gridColumn=== food.style.gridColumn ){
+            row = parseInt( head.style.gridRow ) + 1
+            head.style.gridRow = row
+            if ( head.style.gridRow === food.style.gridRow &&
+                 head.style.gridColumn=== food.style.gridColumn ){
                    food.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
                    food = boxes[ Math.ceil( Math.random()*900) ]
                    food.style.backgroundColor = 'purple' 
@@ -307,14 +505,15 @@ document.onkeydown = startMove = ( key ) => {
         for ( let i = 1; i <= snakeLength; i++ ) {
             tailsArray[i].style.backgroundColor    = 'pink'
             tailsArray[i].style.gridRow            = row - i
-            tailsArray[i].style.gridColumn         = snake.style.gridColumn
+            tailsArray[i].style.gridColumn         = head.style.gridColumn
         }
         },100)
-        snake.style.backgroundColor = 'yellow' 
+        head.style.backgroundColor = 'yellow' 
     }
     else { console.log( 'User pressed non-arrow key' ) }
 }
 
 
+}
 
-
+*/ 
