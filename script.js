b13cf98165babe869ed8896906e6ddbf73517f47
James Jewitt <jamestjewitt@gmail.com>
2022-07-05 12:31:23 -0400

    Increased efficiency when 'snake' hits border walls, added minimal styling

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
  
 
-let grid        = document.querySelector( 'div' )
-let box         = document.createElement( 'div' )
+let grid        = document.querySelector( 'div'  )
+let box         = document.createElement( 'div'  )
+let onOff       = document.querySelector( 'button' )
 let boxes       = []
 let indices     = []
 let path        = []
 let elements    = []
 let snakeLength = 1
+let level       = 1
 let row         = 0
 let column      = 0
 let boxNum      = -1
 let gameOn      = false
+
+
+
+
+onOff.innerText = 'START GAME'
+onOff.addEventListener( 'click', () => {
+
+    onOff.innerText = `Level ${level}`
+    gameOn = true
+
+})
+
 
 
 /* appending tablets to game board */
@@ -51,160 +65,201 @@ elements[0].style.gridColumn = '7'
 elements[0].style.backgroundColor = 'blue'
 console.log( elements )
 
-let goLeft = false
+
+
 
  
 document.onkeydown = startMove = ( key ) => {
+    if ( gameOn === true ) {
+        if ( key.keyCode === 38 ) {
 
-    if ( key.keyCode === 38 ) {
+            //otherMovements.forEach( item => clearInterval( item ) )
+            try { clearInterval( moveDown ) } 
+            catch ( error ) { }
+            try { clearInterval( moveLeft ) } 
+            catch ( error ) { }
+            try { clearInterval( moveRight ) } 
+            catch ( error ) { }
 
-        //otherMovements.forEach( item => clearInterval( item ) )
-        try { clearInterval( moveDown ) } 
-        catch ( error ) { }
-        try { clearInterval( moveLeft ) } 
-        catch ( error ) { }
-        try { clearInterval( moveRight ) } 
-        catch ( error ) { }
+            moveUp = setInterval( () => {
+                
+                row     = parseInt( elements[0].style.gridRow ) - 1
+                column  = parseInt( elements[0].style.gridColumn )
+                
+                path.unshift([ row , column ])
 
-        moveUp = setInterval( () => {
-            row     = parseInt( elements[0].style.gridRow ) - 1
-            column  = parseInt( elements[0].style.gridColumn )
-            path.unshift([ row , column ])
+                if( elements[0].style.gridRow    === powerUp.style.gridRow &&
+                    elements[0].style.gridColumn === powerUp.style.gridColumn ){
 
-            if( elements[0].style.gridRow === '1' ) {
-                elements[0].style.gridColumn = path[0][1] + 1
-            } 
-            else {
-                for( let i = 0; i < snakeLength; i++ ) {
-                    elements[i].style.gridRow         = path[i][0]
-                    elements[i].style.gridColumn      = path[i][1]
-                    elements[i].style.backgroundColor = 'red'
+                    powerUp.style.backgroundColor = 'transparent'
+                    powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
+                    powerUp.style.backgroundColor = 'yellow'
+                    snakeLength += 1
                 }
-            }
 
-            if( elements[0].style.gridRow    === powerUp.style.gridRow &&
-                elements[0].style.gridColumn === powerUp.style.gridColumn ){
+                for ( let i = 0; i < snakeLength; i++ ) {
 
-                powerUp.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
-                powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
-                powerUp.style.backgroundColor = 'yellow'
-                snakeLength += 1
-            }
-
-            if ( elements[0].style.gridRow    === elements[snakeLength-1].style.gridRow && 
-                 elements[0].style.gridColumn === elements[snakeLength-1].style.gridColumn ){
-                    elements[0].style.backgroundColor = 'black'
-                 }
-             
-            
-
-
-
-
-    },100)}
-    if ( key.keyCode === 40  ) {
-
-        try { clearInterval( moveUp ) } 
-        catch ( error ) { }
-        try { clearInterval( moveLeft ) } 
-        catch ( error ) { }
-        try { clearInterval( moveRight ) } 
-        catch ( error ) { }
-
-        moveDown = setInterval( () => {
-            row     = parseInt( elements[0].style.gridRow ) + 1
-            column  = parseInt( elements[0].style.gridColumn )
-            path.unshift([ row , column ])
-
-
-            if( elements[0].style.gridRow === '30' ) {
-                elements[0].style.gridColumn = path[0][1] + 1
-            } 
-            else {
-                for( let i = 0; i < snakeLength; i++ ) {
-                    elements[i].style.gridRow         = path[i][0]
-                    elements[i].style.gridColumn      = path[i][1]
+                    if ( elements[i].style.gridRow === '1' ) {
+                        elements[i].style.gridColumn = path[i][1] + 1 }
+                    else {
+                        elements[i].style.gridColumn = path[i][1] }
+                        
                     elements[i].style.backgroundColor = 'red'
+                    elements[i].style.gridRow         = path[i][0]     
                 }
-            }
-
-            if( elements[0].style.gridRow    === powerUp.style.gridRow &&
-                elements[0].style.gridColumn === powerUp.style.gridColumn ){
-
-                powerUp.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
-                powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
-                powerUp.style.backgroundColor = 'yellow'
-                snakeLength += 1
-            }
-    },100)}
-    if ( key.keyCode === 37 ) {
-
-        try { clearInterval( moveUp ) } 
-        catch ( error ) { }
-        try { clearInterval( moveDown ) } 
-        catch ( error ) { }
-        try { clearInterval( moveRight ) } 
-        catch ( error ) { }
 
 
-        moveLeft = setInterval( () => {
-            row     = parseInt( elements[0].style.gridRow    )  
-            column  = parseInt( elements[0].style.gridColumn ) - 1
-            path.unshift([ row , column ])
 
-            if( elements[0].style.gridColumn === '1' ) {
-                elements[0].style.gridRow = path[0][0] + 1
-            } 
-            else {
-                for( let i = 0; i < snakeLength; i++ ) {
-                    elements[i].style.gridRow         = path[i][0]
-                    elements[i].style.gridColumn      = path[i][1]
+
+
+
+
+
+
+
+
+
+
+                // for ( let i = 0; i < snakeLength; i++ ) {
+                //     if ( column === 1 ) {
+                //         elements[i].style.gridRow    = path[i][0] + 2 
+                //         elements[i].style.gridColumn = path[i][1] 
+                // }
+                //     else if( elements[i].style.gridRow === '1' && 
+                //         (elements[i].style.gridColumn != '1' || elements[i].style.gridColumn != '30')){ 
+                //         elements[i].style.gridColumn = path[i][1] - 1
+                //         elements[i].style.backgroundColor = 'pink'
+                //     }
+                //     else { 
+                //         elements[i].style.gridRow         = path[i][0]  
+                //         elements[i].style.gridColumn      = path[i][1]  
+                //     }  
+                // }
+                // for ( let i = 1; i < snakeLength; i++ ) {
+                //     if( elements[i].style.gridRow    === elements[0].style.gridRow &&
+                //         elements[i].style.gridColumn === elements[0].style.gridColumn )  
+                //         elements[i].style.backgroundColor = 'yellow'
+                //         clearInterval( moveUp )
+                // }
+                
+        },100)}
+        if ( key.keyCode === 40  ) {
+
+            try { clearInterval( moveUp ) } 
+            catch ( error ) { }
+            try { clearInterval( moveLeft ) } 
+            catch ( error ) { }
+            try { clearInterval( moveRight ) } 
+            catch ( error ) { }
+
+            moveDown = setInterval( () => {
+                if ( row != 30 ) {  
+                    row = parseInt( elements[0].style.gridRow ) + 1 }
+                else { row = parseInt( elements[0].style.gridRow )     }
+
+                column  = parseInt( elements[0].style.gridColumn )
+                path.unshift([ row , column ])
+
+                if( elements[0].style.gridRow    === powerUp.style.gridRow &&
+                    elements[0].style.gridColumn === powerUp.style.gridColumn ){
+
+                    powerUp.style.backgroundColor = 'transparent'
+                    powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
+                    powerUp.style.backgroundColor = 'yellow'
+                    snakeLength += 1
+                }
+
+                for ( let i = 0; i < snakeLength; i++ ) {
+
+                    if ( elements[i].style.gridRow === '30' ) {
+                        elements[i].style.gridColumn = path[i][1] + 1 }
+                    else {
+                        elements[i].style.gridColumn      = path[i][1] }
+                        
                     elements[i].style.backgroundColor = 'red'
+                    elements[i].style.gridRow         = path[i][0]     
                 }
-            }
-            if( elements[0].style.gridRow    === powerUp.style.gridRow &&
-                elements[0].style.gridColumn === powerUp.style.gridColumn ){
 
-                powerUp.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
-                powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
-                powerUp.style.backgroundColor = 'yellow'
-                snakeLength += 1
-            }
-    },100)}
-    if ( key.keyCode === 39 ) {
 
-        try { clearInterval( moveUp ) } 
-        catch ( error ) { }
-        try { clearInterval( moveDown ) } 
-        catch ( error ) { }
-        try { clearInterval( moveLeft ) } 
-        catch ( error ) { }
+        },100)}
+        if ( key.keyCode === 37 ) {
 
-        moveRight = setInterval( () => {
-            row     = parseInt( elements[0].style.gridRow    )
-            column  = parseInt( elements[0].style.gridColumn ) + 1
-            path.unshift([ row , column ])
+            try { clearInterval( moveUp ) } 
+            catch ( error ) { }
+            try { clearInterval( moveDown ) } 
+            catch ( error ) { }
+            try { clearInterval( moveRight ) } 
+            catch ( error ) { }
 
-            if( elements[0].style.gridColumn === '30' ) {
-                elements[0].style.gridRow = path[0][0] + 1
-            } 
-            else {
-                for( let i = 0; i < snakeLength; i++ ) {
-                    elements[i].style.gridRow         = path[i][0]
-                    elements[i].style.gridColumn      = path[i][1]
+
+            moveLeft = setInterval( () => {
+                row     = parseInt( elements[0].style.gridRow    )  
+                column  = parseInt( elements[0].style.gridColumn ) - 1
+                path.unshift([ row , column ])
+
+                if( elements[0].style.gridRow    === powerUp.style.gridRow &&
+                    elements[0].style.gridColumn === powerUp.style.gridColumn ){
+
+                    powerUp.style.backgroundColor = 'transparent'
+                    powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
+                    powerUp.style.backgroundColor = 'yellow'
+                    snakeLength += 1
+                }
+
+
+                for ( let i = 0; i < snakeLength; i++ ) {
+
+                    if ( elements[i].style.gridColumn === '1' ) {
+                        elements[i].style.gridRow = path[i][0] + 1 }
+                    else {
+                        elements[i].style.gridRow  = path[i][0] }
+
                     elements[i].style.backgroundColor = 'red'
+                    elements[i].style.gridColumn      = path[i][1]     
                 }
-            }
 
-            if( elements[0].style.gridRow    === powerUp.style.gridRow &&
-                elements[0].style.gridColumn === powerUp.style.gridColumn ){
+        },100)}
+        if ( key.keyCode === 39 ) {
 
-                powerUp.style.backgroundColor = 'rgba( 255,255,255,0.4 )'
-                powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
-                powerUp.style.backgroundColor = 'yellow'
-                snakeLength += 1
-            }
-    },100)}
+            try { clearInterval( moveUp ) } 
+            catch ( error ) { }
+            try { clearInterval( moveDown ) } 
+            catch ( error ) { }
+            try { clearInterval( moveLeft ) } 
+            catch ( error ) { }
+
+            moveRight = setInterval( () => {
+                row     = parseInt( elements[0].style.gridRow    )
+                if ( column != 30 ) {
+                    column = parseInt( elements[0].style.gridColumn ) + 1 }
+                else { column = parseInt( elements[0].style.gridColumn )     }
+
+                path.unshift([ row , column ])
+
+                if( elements[0].style.gridRow    === powerUp.style.gridRow &&
+                    elements[0].style.gridColumn === powerUp.style.gridColumn ){
+
+                    powerUp.style.backgroundColor = 'transparent'
+                    powerUp                       = boxes[ Math.ceil( Math.random()*900) ]
+                    powerUp.style.backgroundColor = 'yellow'
+                    snakeLength += 1
+                }
+
+
+                for ( let i = 0; i < snakeLength; i++ ) {
+
+                    if ( elements[i].style.gridColumn === '30' ) {
+                        elements[i].style.gridRow = path[i][0] + 1 }
+                    else {
+                        elements[i].style.gridRow = path[i][0] }
+
+                    elements[i].style.backgroundColor = 'red'
+                    elements[i].style.gridColumn      = path[i][1]     
+                }
+
+        },100)}
+    }
+
 }
     
 
